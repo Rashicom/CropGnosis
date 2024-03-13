c27c0365e8fd49a1c5695dbea8110db954851d01
Rashicom <rashidkp.484@gmail.com>
2024-03-13 15:11:58 +0530

    table registered in admin

--- apps/authorization/admin.py
+++ apps/authorization/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Accounts
+from .models import Accounts, Address, Otp, AccountSubscription, MentorSubscriptions
 
 
 @admin.register(Accounts)
@@ -16,3 +16,45 @@ class AccountsAdmin(admin.ModelAdmin):
         "is_staff",
         "is_active",
     )
+
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = (
+        "account",
+        "place",
+        "city",
+        "state",
+        "zip_code",
+        "about",
+        "designation",
+        "mentor_fee"
+    )
+
+
+@admin.register(Otp)
+class OtpAdmin(admin.ModelAdmin):
+    list_display = (
+        "account",
+        "otp_code",
+        "is_used"
+    )
+
+
+@admin.register(AccountSubscription)
+class AccountSubscriptionAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "base_plan",
+        "valied_till"
+    )
+
+
+@admin.register(MentorSubscriptions)
+class MentorSubscriptionsAdmin(admin.ModelAdmin):
+    list_display = (
+        "farmer",
+        "mentor",
+        "valied_till"
+    )
+
--- apps/subscription/admin.py
+++ apps/subscription/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from .models import BaseSubscriptionPlans, EssentialFeatures, PaymentTransactions
 
-# Register your models here.
+
+@admin.register(BaseSubscriptionPlans)
+class BaseSubscriptionPlansAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "discription",
+        "total_price",
+        "discounted_price",
+        "crop_field_count",
+        "plan_validity"
+    )
+
+
+@admin.register(EssentialFeatures)
+class EssentialFeaturesAdmin(admin.ModelAdmin):
+    list_display = (
+        "feature",
+        "remark",
+        "feature_price"
+    )
+
+
+@admin.register(PaymentTransactions)
+class PaymentTransactionsAdmin(admin.ModelAdmin):
+    list_display = (
+        "paid_for",
+        "subscription_plan",
+        "addon",
+        "mentor",
+        "amount",
+        "transaction_id",
+        "invoice_pdf",
+        "created_by",
+        "payment_response",
+        "status"
+    )
